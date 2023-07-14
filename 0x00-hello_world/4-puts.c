6b458e13a2211bb4c6be5e6af4c4cf27290d88e7
PeinNotFound <akatsuki_ahmed@yahoo.com>
2023-07-13 17:24:00 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - entry poit
-* return: always 0 (sucsses)
+*main - entry poit
+*return: always 0 (sucsses)
 */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
