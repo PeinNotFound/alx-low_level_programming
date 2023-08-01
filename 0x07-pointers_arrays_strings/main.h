384d4e2eaefeb1e445a59ac7b0df5bcffd565cf4
PeinNotFound <akatsuki_ahmed@yahoo.com>
2023-08-01 08:31:44 -0700

    task 8

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 #endif /* MAIN_H */
