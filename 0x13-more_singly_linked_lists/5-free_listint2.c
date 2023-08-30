8f8ea62e1f69efc7ba4b69ab1a72c15b47d001ef
peinNotFound <akatsuki_ahmed@yahoo.com>
2023-08-29 18:04:05 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 -instruction that  frees a listint_t list
+ *
+ * @head: The header of listint_t list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+}
