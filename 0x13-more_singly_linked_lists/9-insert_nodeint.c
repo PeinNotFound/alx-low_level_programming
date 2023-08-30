2e76e980b570a390ad29ffc4547a51f25514fa6c
peinNotFound <akatsuki_ahmed@yahoo.com>
2023-08-29 18:28:52 -0700

    task 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,109 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - instruction that inserts a new node
+ * at a given position
+ * @head: is the head of the listint_t list
+ * @idx: Index where to insert the new node
+ * @n: is a Data to add to the new added node
+ *
+ * Return: The address of the new node, or NULL if it failed
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *newNode;
+	listint_t *originHead = *head;
+
+	if (head == NULL)
+		return (NULL);
+
+	if (idx == 0)
+		return (add_nodeint(head, n));
+
+	if (idx == listint_len(*head))
+		return (add_nodeint_end(head, n));
+
+	newNode = malloc(sizeof(listint_t));
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->n = n;
+	newNode->next = get_nodeint_at_index(*head, idx);
+	*head = get_nodeint_at_index(*head, idx - 1);
+
+	if (newNode->next == NULL || *head == NULL)
+	{
+		*head = originHead;
+		free(newNode);
+		return (NULL);
+	}
+
+	(*head)->next = newNode;
+	*head = originHead;
+
+	return (newNode);
+}
+/* Helper Function */
+
+/**
+ * get_nodeint_at_index - it gets the nth node of a listint_t linked list
+ * @head: is the head of the listint_t list
+ * @index: is the idndex to search for
+ *
+ * Return: The nth node of a listint_t linked list
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (i = 0; head != NULL && i != index; i++)
+		head = head->next;
+
+	if (i != index)
+		return (NULL);
+
+	return (head);
+}
+/**
+ * add_nodeint - it adds a new node at the beginning of a listint_t list
+ * @head: is the header of listint_t list
+ * @n: is the integer to add to the new added node
+ *
+ * Return: The address of the new element, or NULL if it failed
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newNode;
+
+	newNode = malloc(sizeof(listint_t));
+
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->n = n;
+	newNode->next = *head;
+	*head = newNode;
+
+	return (newNode);
+}
+
+/**
+ * listint_len - Counts the number of elements in a linked listint_t list
+ * @h: The header of listint_t list
+ *
+ * Return: The number of elements in a linked listint_t list.
+ */
+size_t listint_len(const listint_t *h)
+{
+	int numNodes = 0;
+
+	while (h != NULL)
+	{
+		numNodes++;
+		h = h->next;
+	}
+
+	return (numNodes);
+}
