f7db1feaa0cf836e77d7ebd008562dfef28476b7
peinNotFound <akatsuki_ahmed@yahoo.com>
2023-08-31 12:51:34 -0700

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - instruction that printes the
+ * binary representation of a number
+ * @n: is the number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+	const int numOfBits = sizeof(unsigned long int) * 8;
+	unsigned long int mask;
+	int i, zeroSkipper = 0;
+
+	for (i = 0; i < numOfBits; i++)
+	{
+		mask = (n >> (numOfBits - 1 - i)) & 1;
+
+		if (mask == 1)
+		{
+			_putchar('1');
+			zeroSkipper = 1;
+		}
+
+		if (mask == 0 && (zeroSkipper == 1 || i == numOfBits - 1))
+			_putchar('0');
+	}
+}
